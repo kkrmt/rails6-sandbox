aa2c8ca12e9f3aab262ceed0a91c0f7258626c55
kkrmt <kurimoto.kei.721+kkrmt@gmail.com>
2021-08-30 23:30:16 +0900

    GH-1: fix files

--- db/migrate/20210829121748_create_customer.rb
+++ db/migrate/20210829121748_create_customer.rb
@@ -1,17 +1,17 @@
 class CreateCustomer < ActiveRecord::Migration[6.0]
   def change
-    # create_table :customers do |t|
-    #   t.string :email, null: false
-    #   t.string :family_name, null: false
-    #   t.string :given_name, null: false
-    #   t.string :family_name_kana, null: false
-    #   t.string :given_name_kana, null: false
-    #   t.string :gender
-    #   t.date :birthday
-    #   t.string :hashed_password
-    #
-    #   t.timestamps
-    # end
+    create_table :customers do |t|
+      t.string :email, null: false
+      t.string :family_name, null: false
+      t.string :given_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :given_name_kana, null: false
+      t.string :gender
+      t.date :birthday
+      t.string :hashed_password
+
+      t.timestamps
+    end
 
     add_index :customers, [ :family_name_kana, :given_name_kana ]
   end
